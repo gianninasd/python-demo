c9fced47ea0ab4f9a922dea83d25bf5d19e16eff
gianninasd <angelone55@hotmail.com>
2018-11-13 23:56:59 -0500

    sending card request as json

--- cardMain.py
+++ cardMain.py
@@ -1,4 +1,6 @@
 import requests
+import json
+import uuid
 
 # config
 accountId = '1001289630'
@@ -6,16 +8,29 @@ cardUrl = 'https://api.test.paysafe.com/cardpayments/v1/accounts/' + accountId +
 
 apiUser = 'test_assl1'
 apiPass = 'B-qa2-0-5be8832c-0-302c02146cd8d52ddcca8ee8ce57845505ce80cfcab23c6202140fb0300b0bf9a8cc63fc0b7d58e2c0c1b6724130'
-apiKey = 'dGVzdF9hc3NsMTpCLXFhMi0wLTViZTg4MzJjLTAtMzAyYzAyMTQ2Y2Q4ZDUyZGRjY2E4ZWU4Y2U1Nzg0NTUwNWNlODBjZmNhYjIzYzYyMDIxNDBmYjAzMDBiMGJmOWE4Y2M2M2ZjMGI3ZDU4ZTJjMGMxYjY3MjQxMzA='
 headers = {'content-type': 'application/json'}
 
+cardReq = {
+  "merchantRefNum": uuid.uuid1().hex,
+  "amount": 100,
+  "card": {
+    "cardNum": "4111111111111111",
+    "cardExpiry": {
+      "month": "10",
+      "year": "2020"
+    }
+  },
+  "billingDetails": {
+  	"zip": "H8P1K1"
+  }
+}
+
 # send the request
-resp = requests.post(cardUrl, headers=headers, auth=(apiUser, apiPass), data={}) 
-# auth=(apiUser, apiPass)
-#.get('https://google.ca')
+print('Sending: ' + json.dumps(cardReq))
+resp = requests.post(cardUrl, headers=headers, auth=(apiUser, apiPass), data=json.dumps(cardReq)) 
 
 # process response
-print('status: ' + str(resp.status_code) )
+print('Status: ' + str(resp.status_code) )
 
 if resp.status_code == 200:
-  print('response body: ' + resp.json() )
+  print('response body: ' + str(resp.json()) )
