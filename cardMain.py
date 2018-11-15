2da597bc29103c2caaf666eddf2b0295076719f4
gianninasd <angelone55@hotmail.com>
2018-11-14 19:45:52 -0500

    cleaned up result output

--- cardMain.py
+++ cardMain.py
@@ -9,7 +9,8 @@ headers = {'content-type': 'application/json'}
 
 cardReq = {
   "merchantRefNum": uuid.uuid1().hex,
-  "amount": 100,
+  "amount": 1500,
+  "settleWithAuth": True,
   "card": {
     "cardNum": "4111111111111111",
     "cardExpiry": {
@@ -23,7 +24,8 @@ cardReq = {
 }
 
 # send the request
-print('Sending: ' + json.dumps(cardReq))
+#print('Sending: ' + json.dumps(cardReq))
+print('Sending reference ' + cardReq['merchantRefNum'] + ' with amount ' + str(cardReq['amount']))
 resp = requests.post(cardUrl, headers=headers, auth=(config['apiUser'], config['apiPass']), data=json.dumps(cardReq)) 
 
 # process response
@@ -31,8 +33,16 @@ print('Status: ' + str(resp.status_code) )
 
 if resp.status_code == 200:
   #print('response body: ' + str(resp.json()) )
+  print('--- SUCCESSFUL ---')
   print('id: ' + str(resp.json()['id']) )
   print('status: ' + str(resp.json()['status']) )
   print('authCode: ' + str(resp.json()['authCode']) )
   print('avsResponse: ' + str(resp.json()['avsResponse']) )
   print('cvvVerification: ' + str(resp.json()['cvvVerification']) )
+elif resp.status_code == 400:
+  errorObj = resp.json()['error']
+  print('--- BAD REQUEST ---')
+  print('Failed with error code: ' + errorObj['code'] + ' with message: ' + errorObj['message'])
+  print('Details: ' + str(errorObj['details']))
+elif resp.status_code == 500:
+  print('--- OOPS, SERVER ERROR! ---')
