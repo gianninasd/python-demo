ad2249a2f456dc4926e2112ba936c4adff4a9ff5
gianninasd <angelone55@hotmail.com>
2018-11-25 18:47:04 -0500

    updated load runner test data

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ config = {
   'url': 'https://api.test.paysafe.com/cardpayments/v1/accounts/',
   'apiUser': 'test_assl1',
   'apiPass': 'B-qa2-0-5be8832c-0-302c02146cd8d52ddcca8ee8ce57845505ce80cfcab23c6202140fb0300b0bf9a8cc63fc0b7d58e2c0c1b6724130',
-  'user': 'assl1',
-  'pass': 'Test1234'
+  'runnerMins': 2,
+  'maxThreads': 10,
+  'maxRequests': 5000
 }
--- runner.py
+++ runner.py
@@ -10,6 +10,7 @@ import logging.config
 import datetime
 import concurrent.futures
 import uuid
+import platform
 
 # setup application logging
 logging.config.fileConfig('logging.conf')
@@ -32,46 +33,58 @@ def processReq(line):
 def processRes(result):
   logging.info(result.toString())
 
-somedata = ['jim7025,1500,4111111111111111,10,2020,Rick,Hunter,rick@sdf3.com,M5H 2N2',
-  'jim7025,5,4111111111111111,10,2020,Rick,Hunter,rick@sdf3.com,M5H 2N2',
-  'jim7025,1500,4111111111111111,10,2020,Rick,Hunter,rick@sdf3.com,M5H 2N2',
-  'jim7025,1500,4111111111111111,10,2020,Rick,Hunter,rick@sdf3.com,M5H 2N2']
-
 requestCnt = 0
 successCnt = 0
 failedCnt = 0
 startTime = datetime.datetime.now()
 rightnow = datetime.datetime.now()
+maxThreads = int(config['maxThreads'])
+fileName = 'runner_sample.csv'
+sampleData = []
 
-# start thread pool with maximum number of threads
-with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-  allFutures = []
+logging.info('Python ' + platform.python_version() + ' File Processor running on ' + str(platform.system()) + ' ' + str(platform.release()))
+logging.info('Load test running for ' + str(config['runnerMins']) + ' with ' + str(config['maxRequests']) + ' threads')
 
-  while ((rightnow - startTime).total_seconds() / 60) < 2 and requestCnt < 2500:
-    for i in range(100):
-      for rec in somedata:
-        future = executor.submit(processReq, rec)
-        allFutures.append(future)
+try:
+  # open file and load each record into memory
+  srcFile = open(fileName,'rt')
+  for line in srcFile:
+    sampleData.append(line)
 
-    # loop thru each completed thread and handle result
-    for future in concurrent.futures.as_completed(allFutures):
-      if future.result().decision == 'SUCCESS':
-        successCnt += 1
-      elif future.result().decision == 'FAILED':
-        failedCnt += 1
+  # start thread pool with maximum number of threads
+  with concurrent.futures.ThreadPoolExecutor(max_workers=maxThreads) as executor:
+    allFutures = []
 
-      processRes(future.result())
-      requestCnt += 1
+    while ((rightnow - startTime).total_seconds() / 60) < int(config['runnerMins']) and requestCnt < int(config['maxRequests']):
+      for i in range(100):
+        for rec in sampleData:
+          future = executor.submit(processReq, rec)
+          allFutures.append(future)
 
-    rightnow = datetime.datetime.now()
+      # loop thru each completed thread and handle result
+      for future in concurrent.futures.as_completed(allFutures):
+        if future.result().decision == 'SUCCESS':
+          successCnt += 1
+        elif future.result().decision == 'FAILED':
+          failedCnt += 1
 
-endTime = datetime.datetime.now()
+        processRes(future.result())
+        requestCnt += 1
 
-x = endTime - startTime
+      rightnow = datetime.datetime.now()
 
-logging.info('startTime: ' + str(startTime))
-logging.info('endTime: ' + str(endTime))
-logging.info('elapsed time: ' + str(x.total_seconds() / 60))
+  endTime = datetime.datetime.now()
+  elapsedMins = (endTime - startTime).total_seconds() / 60
+  pctSuccess = round((successCnt / (successCnt + failedCnt))*100)
 
-logging.info('Processed ' + str(requestCnt) + ' record(s) in ' + str(endTime - startTime) \
-    + ' - ' + str(successCnt) + ' succeeded, ' + str(failedCnt) + ' failed')
+  logging.info('startTime: ' + str(startTime))
+  logging.info('endTime: ' + str(endTime))
+  logging.info('elapsed time: ' + str(elapsedMins))
+
+  logging.info('Processed ' + str(requestCnt) + ' record(s) in ' + str(endTime - startTime) \
+      + ' - ' + str(successCnt) + ' succeeded (' + str(pctSuccess) + '%), ' + str(failedCnt) + ' failed')
+
+except FileNotFoundError as ex:
+  logging.error('File [' + fileName + '] not found')
+except Exception as ex:
+  logging.exception('Unknown error occured!?')
