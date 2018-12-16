dcc265fc89055c6adb6a2426e024adbbeb1e5aae
gianninasd <angelone55@hotmail.com>
2018-12-16 15:09:31 -0500

    updated unit test

--- dg/CardRequest.py
+++ dg/CardRequest.py
@@ -11,20 +11,6 @@ class CardRequest:
   lastName = ''
   email = ''
 
-  # parses a comma seperated string into each field
-  def parse(self, line):
-    tokens = str(line).strip().split(',')
-
-    self.ref = str(tokens[0]).strip()
-    self.amount = str(tokens[1]).strip()
-    self.cardNbr = str(tokens[2]).strip()
-    self.cardExpMth = str(tokens[3]).strip()
-    self.cardExpYear = str(tokens[4]).strip()
-    self.firstName = str(tokens[5]).strip()
-    self.lastName = str(tokens[6]).strip()
-    self.email = str(tokens[7]).strip()
-    self.zipCode = str(tokens[8]).strip()
-
   def toString(self):
     return 'ref=' + str(self.ref) + \
       ',amount=' + str(self.amount) + \
--- test/testCardRequest.py
+++ test/testCardRequest.py
@@ -6,13 +6,10 @@ class TestCardRequest(unittest.TestCase):
 
   def test_parse(self):
     req = CardRequest()
-    req.parse('ref1,15.00,4111222233334444,10,2020,Rick,Hunter,rick@sdf3.com,90210')
-    self.assertEqual(req.ref, 'ref1')
-    self.assertEqual(req.amount, '15.00')
-    self.assertEqual(req.cardNbr, '4111222233334444')
-    self.assertEqual(req.cardExpMth, '10')
-    self.assertEqual(req.cardExpYear, '2020')
-    self.assertEqual(req.zipCode, '90210')
-    self.assertEqual(req.firstName, 'Rick')
-    self.assertEqual(req.lastName, 'Hunter')
-    self.assertEqual(req.email, 'rick@sdf3.com')
+    req.ref = 'ref1'
+    req.amount = '1500'
+    req.firstName = 'Rick'
+    req.lastName = 'Hunter'
+    req.email = 'rick@sdf3.com'
+    req.cardNbr = '4111222233334444'
+    self.assertEqual(req.toString(), 'ref=ref1,amount=1500,firstName=Rick,lastName=Hunter,email=rick@sdf3.com,cardNbr=XXXX4444')
