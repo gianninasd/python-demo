e2d0618e37b9691ae2e2ff50452298bb1fd4a092
gianninasd <angelone55@hotmail.com>
2018-12-22 18:06:45 -0500

    always processing folder

--- fileLoader.py
+++ fileLoader.py
@@ -3,6 +3,7 @@
 from dg.FileService import FileService, DupeFileException, SecretKeyNotFoundException
 from config import config
 
+import time
 import os
 import platform
 import logging
@@ -23,28 +24,34 @@ try:
   workingDir = config['workingDir']
   service = FileService(secretKey)
 
-  # loop for each file in the working directory
-  for fullFileName in os.listdir(workingDir):
-    if os.path.isfile(workingDir + '/' + fullFileName):
-      try:
-        fileName = service.extractFileName(fullFileName)
-        fileId = service.create(workingDir, fullFileName)
-        logging.info('Processing records for file id ' + str(fileId))
+  while True:
+    logging.info('Processing files in ' + workingDir)
 
-        # open file and loop for each line
-        with open(workingDir + '/' + fullFileName,'rt') as srcFile:
-          for line in srcFile:
-            service.storeRecord(fileId, line)
-            cnt += 1
+    # loop for each file in the working directory
+    for fullFileName in os.listdir(workingDir):
+      if os.path.isfile(workingDir + '/' + fullFileName):
+        try:
+          fileName = service.extractFileName(fullFileName)
+          fileId = service.create(workingDir, fullFileName)
+          logging.info('Processing [' + fullFileName + '] records with file id ' + str(fileId))
 
-        logging.info('Finished storing ' + str(cnt) + ' records for file ' + str(fileId))
-        service.createAck(workingDir,fileName,'0','File received')
-      
-      except FileNotFoundError as ex:
-        logging.error('File [' + fullFileName + '] not found')
-      except DupeFileException as ex:
-        logging.error('File [' + fullFileName + '] already uploaded in the last 24 hrs')
-        service.createAck(workingDir,fileName,'-1','Duplicate file')
+          # open file and loop for each line
+          with open(workingDir + '/' + fullFileName,'rt') as srcFile:
+            for line in srcFile:
+              service.storeRecord(fileId, line)
+              cnt += 1
+
+          logging.info('Finished storing ' + str(cnt) + ' records for file id ' + str(fileId))
+          service.createAck(workingDir,fileName,'0','File received')
+        
+        except FileNotFoundError as ex:
+          logging.warning('File [' + fullFileName + '] not found')
+        except DupeFileException as ex:
+          logging.warning('File [' + fullFileName + '] already uploaded in the last 24 hrs')
+          service.createAck(workingDir,fileName,'-1','Duplicate file')
+    
+    logging.info('Going to sleep for 120s...')
+    time.sleep(120) 
 
 except SecretKeyNotFoundException as ex:
   logging.error('Encryption key not found in environment variable DG_SECRET_KEY')
