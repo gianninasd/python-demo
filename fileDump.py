2acb673ea63482abcb17e2abb21075f0f9092e5e
gianninasd <angelone55@hotmail.com>
2019-01-12 13:31:55 -0500

    added log info

--- fileDump.py
+++ fileDump.py
@@ -5,5 +5,7 @@ from dg.RecordDAO import RecordDAO
 dao = RecordDAO()
 recs = dao.getAll()
 
+print(str(len(recs)) + ' record(s) retrieved')
+
 for rec in recs:
   print(str(rec.guid) + ' last modified on ' + str(rec.modificationDate) + ' status is ' + rec.status)
