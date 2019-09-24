7ab87849950fcd38bcdf12f731e9fdfc4030c9b1
Jimmy G <angelone55@hotmail.com>
2019-09-24 19:47:07 -0400

    minor update

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # application config information
 config = {
-  'accountId': '1001289630',
+  'accountId': 'xxx',
   'url': 'https://api.test.paysafe.com/cardpayments/v1/accounts/',
-  'apiUser': 'test_assl1',
-  'apiPass': 'B-qa2-0-5be8832c-0-302c02146cd8d52ddcca8ee8ce57845505ce80cfcab23c6202140fb0300b0bf9a8cc63fc0b7d58e2c0c1b6724130',
+  'apiUser': 'xxx',
+  'apiPass': 'xxx',
   'workingDir': 'working',
   'maxThreads': 5
 }
